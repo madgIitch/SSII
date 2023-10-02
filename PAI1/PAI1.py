e1b827eb9393da4756bef5867f5cd1feac7929d3
madgIitch <peorrr9@gmail.com>
2023-10-02 19:18:19 +0200

    database en lugar de txt

--- PAI1/PAI1.py
+++ PAI1/PAI1.py
@@ -1,22 +1,42 @@
 import os
 import hashlib
+import sqlite3
 
 directorio = r"C:\Users\peorr\OneDrive - UNIVERSIDAD DE SEVILLA\Universidad\4º Curso\1º Cuatrimestre\SSII"
-directorioHash = r"C:\Users\peorr\OneDrive\Documentos\GitHub\SSII\hash.txt"
+directorioDB = r"C:\Users\peorr\OneDrive\Documentos\GitHub\SSII\hashes.db"
 
-def hashFiles(path, pathHash):
+def create_database(connection):
+    cursor = connection.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS hashes
+                      (file_path TEXT PRIMARY KEY, hash_value TEXT)''')
+    connection.commit()
+
+def hashFiles(path, db_path):
     file_list = []
-    with open(pathHash, "w") as h:  # Abre el archivo de hash en modo escritura (reemplaza el archivo existente)
-        for root, dirs, files in os.walk(path):
-            for file in files:
-                file_path = os.path.join(root, file)
-                file_list.append(file_path)
-                with open(file_path, "rb") as f:  # Abre el archivo en modo binario
-                    file_hash = hashlib.sha1()
-                    while chunk := f.read(8192):
-                        file_hash.update(chunk)
-                    h.write(f"{file_path} {file_hash.hexdigest()}\n")
+    conn = sqlite3.connect(db_path)
+    
+    try:
+        create_database(conn)
+        
+        with conn:
+            cursor = conn.cursor()
+            for root, dirs, files in os.walk(path):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    file_list.append(file_path)
+                    
+                    with open(file_path, "rb") as f:
+                        file_hash = hashlib.sha1()
+                        while chunk := f.read(8192):
+                            file_hash.update(chunk)
+                            
+                        cursor.execute("INSERT OR REPLACE INTO hashes (file_path, hash_value) VALUES (?, ?)", (file_path, file_hash.hexdigest()))
+    except sqlite3.Error as e:
+        print("Error:", e)
+    finally:
+        conn.close()
+    
     return file_list
 
-file_list = hashFiles(directorio, directorioHash)
-print("Hashes calculados y almacenados en el archivo 'hash.txt'.")
+file_list = hashFiles(directorio, directorioDB)
+print("Hashes calculados y almacenados en la base de datos 'hashes.db'.")
