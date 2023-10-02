f0c9a970a23c3e28b777778735f9c9c0d66c1368
madgIitch <peorrr9@gmail.com>
2023-10-02 19:07:04 +0200

    actualizacion
    ya calcula el hash de los archivos del directorio seleccionado

--- PAI1/PAI1.py
+++ PAI1/PAI1.py
@@ -1,35 +1,22 @@
-#Sistema HIDS para la deteccion de intrusiones en sistemas Windows
-
 import os
-import sys
-import time
 import hashlib
 
 directorio = r"C:\Users\peorr\OneDrive - UNIVERSIDAD DE SEVILLA\Universidad\4º Curso\1º Cuatrimestre\SSII"
+directorioHash = r"C:\Users\peorr\OneDrive\Documentos\GitHub\SSII\hash.txt"
 
-pathHash = "C:/Users/Usuario/Desktop/SSII/PAI/PAI1/hash.txt"
-
-
-
-#Read a path and return a list with all files in the path
-def readPath(path):
+def hashFiles(path, pathHash):
     file_list = []
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            file_list.append(os.path.join(root, file))
+    with open(pathHash, "w") as h:  # Abre el archivo de hash en modo escritura (reemplaza el archivo existente)
+        for root, dirs, files in os.walk(path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                file_list.append(file_path)
+                with open(file_path, "rb") as f:  # Abre el archivo en modo binario
+                    file_hash = hashlib.sha1()
+                    while chunk := f.read(8192):
+                        file_hash.update(chunk)
+                    h.write(f"{file_path} {file_hash.hexdigest()}\n")
     return file_list
 
-
-#Por cada fichero que nos devuelve readPath, calculamos su hash y lo guardamos en un fichero
-
-def hashFiles(file_list):
-    f = open(pathHash, "w")
-    for file in file_list:
-        try:
-            hash = hashlib.sha256(open(file, "rb").read()).hexdigest()
-            f.write(file + " " + hash + "\n")
-        except:
-            print("Error al calcular el hash del fichero " + file)
-    f.close()
-
-hashFiles(readPath(directorio))
+file_list = hashFiles(directorio, directorioHash)
+print("Hashes calculados y almacenados en el archivo 'hash.txt'.")
